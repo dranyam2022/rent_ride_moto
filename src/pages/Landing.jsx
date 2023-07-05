435f9b2312de30c85e924320135331d854e975e3
dranyam2022 <maynardescalante@gmail.com>
2023-07-05 17:59:00 +0800

    completed landing page layout

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Landing = () => {
-  return <div className="bg-[url()]">Landing...</div>;
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-y-44 bg-[url('/src/assets/landing_moto.jpg')] bg-cover  bg-center bg-no-repeat px-5 py-14 text-white">
+      <h1 className="text-4xl font-bold">
+        Find and rent motorcycles and scooters.
+      </h1>
+      <button className="rounded-lg bg-[#3083FF] px-14 py-4 font-semibold">
+        Let's Go!
+      </button>
+    </div>
+  );
 };
 
 export default Landing;
